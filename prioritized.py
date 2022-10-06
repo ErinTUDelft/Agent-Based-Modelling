20d2ba669f25a63e9a271d678307ec4927d35c2c
ErinTUDelft <93988345+ErinTUDelft@users.noreply.github.com>
2022-10-06 09:47:45 +0200

    Update prioritized.py

--- prioritized.py
+++ prioritized.py
@@ -1,5 +1,8 @@
 import time as timer
+import random
 from single_agent_planner import compute_heuristics, a_star, get_sum_of_cost
+from itertools import permutations
+from single_agent_planner import get_sum_of_cost
 
 
 class PrioritizedPlanningSolver(object):
@@ -17,6 +20,8 @@ class PrioritizedPlanningSolver(object):
         self.num_of_agents = len(goals)
 
         self.CPU_time = 0
+        
+        
 
         # compute heuristics for the low-level search
         self.heuristics = []
@@ -28,25 +33,75 @@ class PrioritizedPlanningSolver(object):
 
         start_time = timer.time()
         result = []
-        constraints = [{'agent': 0,
-                        'loc': [(1,4)],
-                        'timestep': 5},
-                       {'agent': 1,
-                        'loc': [(1,2), (1,3)],
-                        'timestep': 0},
-                       {'agent': 1,
-                        'loc': [(1,2), (1,3)],
-                        'timestep': 2},
-                       {'agent': 1,
-                        'loc': [(1,2), (1,3)],
-                        'timestep': 1}]
-
-        for i in range(self.num_of_agents):  # Find path for each agent
-            path = a_star(self.my_map, self.starts[i], self.goals[i], self.heuristics[i],
-                          i, constraints)
-            if path is None:
-                raise BaseException('No solutions')
+        constraints = []
+        predefined_constraints = [{'agent': 1,
+                        'loc': [(7,2),(6,2)],
+                        'timestep': 1}, {'agent': 1,
+                                        'loc': [(7,2),(6,2)],
+                                        'timestep': 2}, {'agent': 3,
+                                                        'loc': [(7,2),(6,2)],
+                                                        'timestep': 3},{'agent': 3,
+                                                                        'loc': [(7,2),(6,2)],
+                                                                        'timestep': 4}, {'agent': 3,
+                                                                                        'loc': [(7,2),(6,2)],
+                                                                                        'timestep': 5}]
+                        
+                       
+                       
+                       
+                     
+                                                                                                                                                 
+        constraints = [] #predefined_constraints
+        # constraints.append({'agent': 3,
+        #                 'loc': [(6,2)],
+        #                 'timestep': 3})
+        #print("constraints before: ", constraints)
+        #print("num of agents", self.num_of_agents)
+        #print("starts", self.starts) # permutaties van self.start
+      # for start in starts:
+      #     i = 0
+      
+        self.perm = permutations(range(self.num_of_agents))
+        print("perm", self.perm)
+        for startverzameling in list(self.perm):
+            print(startverzameling)
+            constraints = []
+            Uitgang = 0
+            
+            for iterable in range(self.num_of_agents):  # Find path for each agent
+                #print(f"Now cycle {i} begins")
+                
+                i = startverzameling[iterable]
+                path = a_star(self.my_map, self.starts[i], self.goals[i], self.heuristics[i],
+                              i, constraints)
+    
+                if path is None:
+                    #raise BaseException('No solutions')
+                    Uitgang = 1
+                    break
+                    
+                    
+                    
+                j = 0
+        
+                
+                while j < (len(path)-1): # A path for one agent was found using A*, this
+                                         # This now gets appended as constraints for the next agents
+                    constraint = {'agent':  i, 'loc': [path[j],path[j+1]], 'timestep' : j}
+                    j = j+1
+                    constraints.append(constraint)
+                #print("constraints after path: ", constraints, "\n")
+            
+            
+                  
+            if Uitgang == 1:
+                continue
+            #print("constraints: ", constraints)
+            
             result.append(path)
+            cost = get_sum_of_cost(path)
+            print("cost: ", cost)
+            #print("path:", path)
 
             ##############################
             # Task 2: Add constraints here
